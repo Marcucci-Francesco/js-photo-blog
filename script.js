d55ef6a1300d96e29e6778f54e51ed75c88320ba
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-05 12:39:44 +0100

    feat: mileston 2-3 completed

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 **Bonus**
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
 */
+
+
+
+// Dopo aver testato la chiamata all'endpoint di JSON ho verificato che gli elementi utili sono il title e l'url, da qui salvo in una const la chiamata ed essendo un endpoint di json placeholder vado ad usare poi fetch.
+
+const endPoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
+
+
+// Richiamo il file json per poi andare a leggere i dati, vado poi a richiamare l'id di dove andrò ad inserire le cards per assegnargli una stringa vuota in modo da resettarla, dopodiche faccio un forEach per iterare i dati per poi richiamare la funzione di stampa delle cards in modo tale da stamapre una card ad ogni iterazione dell'array di oggetti.
+
+fetch(endPoint)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    document.getElementById('row-cards').innerHTML = '';
+    data.forEach(summer => printCards(summer));
+  })
+
+
+  // Mi creo una funzione per stampare le cards in pagina, andando a destrutturare gli elementi che mi occorrono con cui andro poi a sostituire i paramentri nell'html delle cards, richiamando prima l'id di dove le andrò ad inserire.
+  
+  function printCards(summer){
+    const {title, url} = summer;
+
+    document.getElementById('row-cards').innerHTML += `
+    <div class="col-3">
+      <div class="fm-cards text-center position-relative">
+        <img src="assets_day1/img/pin.svg" alt="" class="img-point">
+        <img src="${url}" alt="">
+        <div>
+          <p>${title}</p>
+        </div>
+      </div>
+    </div>`
+  }
